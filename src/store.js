e31c8bf6d024bad858f58048e49e832fc8ebdc19
Nayara Alves <nayara.nap@gmail.com>
2018-06-30 18:10:33 -0300

    update store mutations, changed splice to filter. Fixes #2

--- src/store.js
+++ src/store.js
@@ -10,9 +10,7 @@ const store = new Vuex.Store({
   },
   mutations: {
     initialiseStore(state) {
-			// Check if the ID exists
 			if(localStorage.getItem('store')) {
-				// Replace the state object with the stored item
 				this.replaceState(
 					Object.assign(state, JSON.parse(localStorage.getItem('store')))
 				);
@@ -23,22 +21,17 @@ const store = new Vuex.Store({
       state.lastId++;
     },
     updateTask (state, params) {
-      console.log(params.id);
-      
-      Object.assign(state.tasks[params.id], {
-        [params.field]: params.value
-      });
+      const item = state.tasks.find(element => element.id == params.id);
+      Object.assign( item , { [params.field]: params.value });
     },
-    deleteTask(state, index) {
-      state.tasks.splice(index, 1);
+    deleteTask(state, param) {
+      state.tasks = state.tasks.filter(task => task.id !== param.id)
     }
   },
   actions: {}
 });
 
-// Subscribe to store updates
 store.subscribe((mutation, state) => {
-	// Store the state object as a JSON string
 	localStorage.setItem('store', JSON.stringify(state));
 });
 
